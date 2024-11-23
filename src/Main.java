f769a2715c39e81cb88e9754b5073160a3aa4718
RomS2002 <romsyt2002@gmail.com>
2024-11-23 13:09:52 +0300

    Working test version

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import serializable.Message;
+import serializable.User;
+
 import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -17,17 +22,95 @@ public class Main {
             in = new ObjectInputStream(socket.getInputStream());
             System.out.println("Соединение успешно установлено!");
 
-            out.write("createUser kane 123456\n".getBytes());
+            out.write("login yal ababab\n".getBytes());
             out.flush();
 
             Scanner scanner = new Scanner(in);
             String s = scanner.next();
             System.out.println(s);
-            s = scanner.next();
-            System.out.println(s);
 
-        } catch(IOException e) {
+            out.write("getUsers\n".getBytes(StandardCharsets.UTF_8));
+            out.flush();
+
+            List<User> users;
+
+            try {
+                users = (List<User>) in.readObject();
+                System.out.println(users.getFirst().getUserName());
+            } catch(ClassNotFoundException e) {
+                System.err.println("ERROR: ошибка получения результата getUsers");
+                return;
+            }
+
+            out.write("getMessagesInChat\n".getBytes(StandardCharsets.UTF_8));
+            out.flush();
+            out.writeObject(users.getFirst().getUserID());
+            out.flush();
+
+            List<Message> messages;
+
+            try {
+                messages = (List<Message>) in.readObject();
+                System.out.println(messages);
+            } catch(ClassNotFoundException e) {
+                System.err.println("ERROR: ошибка получения результата getMessagesInChat");
+            }
+
+            Message message = new Message("yal", "yal", "Hello!");
+            out.write("sendMessage\n".getBytes(StandardCharsets.UTF_8));
+            out.flush();
+            out.writeObject(message);
+            out.flush();
+
+            String response = scanner.next();
+            System.out.println(response);
+
+            out.write("getMessagesInChat\n".getBytes(StandardCharsets.UTF_8));
+            out.flush();
+            out.writeObject(users.getFirst().getUserID());
+            out.flush();
+
+            try {
+                messages = (List<Message>) in.readObject();
+                System.out.println(messages);
+                messages.forEach(e -> System.out.println(e.getText()));
+            } catch(ClassNotFoundException e) {
+                System.err.println("ERROR: ошибка получения результата getMessagesInChat");
+                return;
+            }
+
+            out.write("deleteMessage\n".getBytes(StandardCharsets.UTF_8));
+            out.flush();
+            out.writeObject(users.getFirst().getUserID());
+            out.writeObject(messages.getFirst().getId());
+            out.flush();
+
+            response = scanner.next();
+            System.out.println(response);
+
+            out.write("getMessagesInChat\n".getBytes(StandardCharsets.UTF_8));
+            out.flush();
+            out.writeObject(users.getFirst().getUserID());
+            out.flush();
+
+            try {
+                messages = (List<Message>) in.readObject();
+                System.out.println(messages);
+                messages.forEach(e -> System.out.println(e.getText()));
+            } catch(ClassNotFoundException e) {
+                System.err.println("ERROR: ошибка получения результата getMessagesInChat");
+            }
+
+            out.write("disconnect\n".getBytes(StandardCharsets.UTF_8));
+            out.flush();
+        }
+        catch(OptionalDataException e) {
+            e.printStackTrace();
+            System.out.println(e.length);
+        }
+        catch(IOException e) {
             System.out.println("Ошибка установки соединения");
+            e.printStackTrace();
         }
     }
 
